ad7181c67106997b48c960d022520f627274d9a7
DlbrinAzad <69488118+dlbrin@users.noreply.github.com>
2025-03-06 00:00:22 +0300

    Update MultiLanguageInput.php

--- src/View/Components/MultiLanguageInput.php
+++ src/View/Components/MultiLanguageInput.php
@@ -1,7 +1,8 @@
 <?php
+namespace App\View\Components;
 
-
-use DilbrinAzad\LaravelMultiLang\Src\Models\Language;
+use App\Models\Language;
+use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
