838a69c3653281f744ba0c9eb8cfcc5ffd09703b
DlbrinAzad <69488118+dlbrin@users.noreply.github.com>
2025-03-05 23:53:10 +0300

    Update LaravelMultiLangServiceProvider.php

--- src/Providers/LaravelMultiLangServiceProvider.php
+++ src/Providers/LaravelMultiLangServiceProvider.php
@@ -11,37 +11,16 @@ class LaravelMultiLangServiceProvider extends ServiceProvider
         // Load Migrations
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
-       $this->publishes([
+        $this->publishes([
             __DIR__.'/../../config/multilang.php' => config_path('multilang.php'),
-        ], 'config');
-
-        $this->publishes([
             __DIR__.'/../../database/migrations' => database_path('migrations'),
-        ], 'migrations');
-
-        $this->publishes([
+             __DIR__.'/../Models' => app_path('Models'),
             __DIR__.'/../../database/seeders' => database_path('seeders'),
-        ], 'seeders');
-
-        $this->publishes([
-            __DIR__.'/../Models' => app_path('Models'),
-        ], 'models');
-
-        $this->publishes([
             __DIR__.'/../Http/Controllers' => app_path('Http/Controllers'),
-        ], 'controllers');
-
-        $this->publishes([
+             __DIR__.'/../Traits' => app_path('Traits'),
             __DIR__.'/../../resources/views' => resource_path('views'),
-        ], 'views');
-
-        $this->publishes([
             __DIR__.'/../View/Components' => app_path('View/Components'),
-        ], 'components');
-
-        $this->publishes([
-            __DIR__.'/../Traits' => app_path('Traits'),
-        ], 'traits');
+        ], 'multilang-resources');
     }
 
     public function register()
