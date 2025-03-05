e72a9080d435eeb635fefd1dbc7a19bf3b153435
DlbrinAzad <69488118+dlbrin@users.noreply.github.com>
2025-03-05 22:53:36 +0300

    Update LaravelMultiLangServiceProvider.php

--- src/Providers/LaravelMultiLangServiceProvider.php
+++ src/Providers/LaravelMultiLangServiceProvider.php
@@ -46,6 +46,6 @@ class LaravelMultiLangServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/multilang.php', 'multilang');
+        $this->mergeConfigFrom(__DIR__.'/../../config/multilang.php', 'multilang');
     }
 }
